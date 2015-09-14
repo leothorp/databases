dd9ceaba9e314f9cbba884c7e51dc08096e00f0d
leothorp <leo.thorp1@gmail.com>
2015-09-14 12:30:17 -0500

    basic reqs complete

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,74 +1,40 @@
 var models = require('../models');
 var bluebird = require('bluebird');
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('chat', 'root,' 'MalTwi288'); 
-
-var user = sequelize.define('user', {
-  username: Sequelize.STRING
-});
-
-var message = sequelize.define('message' {
-  text: Sequelize.STRING,
-  roomname: Sequelize.STRING
-});
-
-user.hasMany(message);
-message.belongsTo(user);
-sequelize.sync();
-
+ 
 module.exports = {
   messages: {
-    get: function(req, res) {},
+    get: function (req, res) { // a function which handles a get request for all messages
+      models.messages.get(function(result) {
+        //console.log(result);
+        res.send(JSON.stringify({ results: result}));
+      });
+    },
 
-    post: function(req, res) {}
+//insert into database, get the values from the json object in the request.
+    post: function (req, res) { // a function which handles posting a message to the database
+      console.log('test');
+      console.log('request body in controllers: ', req.body);
+      var callback = function(result){
+        console.log('Messages added');
+        res.send('Messages added to database');
+        console.log('result: ', result);
+      };
+      models.messages.post(req.body, callback);
+    }
   },
-
   users: {
-    get: function(req, res) {},
+    // Ditto as above
+    get: function (req, res) {},
+    post: function (req, res) {
+      console.log(req.body.username);
+      //var username = req.username;
+      var callback = function(result) {
+        res.send('User added to database with ID: ' + result.id);
+      }  
+      models.users.post(req.body.username, callback);
 
-    post: function(req, res) {}
+    }
+
   }
 };
-
-
-
-
-
-//old version, sans Sequelize
-// module.exports = {
-//   messages: {
-//     get: function (req, res) { // a function which handles a get request for all messages
-//       models.messages.get(function(result) {
-//         //console.log(result);
-//         res.send(JSON.stringify({ results: result}));
-//       });
-//     },
-
-// //insert into database, get the values from the json object in the request.
-//     post: function (req, res) { // a function which handles posting a message to the database
-//       console.log('test');
-//       console.log('request body in controllers: ', req.body);
-//       var callback = function(result){
-//         console.log('Messages added');
-//         res.send('Messages added to database');
-//         console.log('result: ', result);
-//       };
-//       models.messages.post(req.body, callback);
-//     }
-//   },
-//   users: {
-//     // Ditto as above
-//     get: function (req, res) {},
-//     post: function (req, res) {
-//       console.log(req.body.username);
-//       //var username = req.username;
-//       var callback = function(result) {
-//         res.send('User added to database with ID: ' + result.id);
-//       }  
-//       models.users.post(req.body.username, callback);
-
-//     }
-
-//   }
-// };
 
